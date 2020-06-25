4604c8aeda1369a129747ab90693f88832c66bff
awis-mbp <awis@awislabs.com>
2020-06-25 21:04:16 +0800

    Initial Commit

---
+++ MyTodoList/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MyTodoList
+//
+//  Created by Awis Alkarni on 25/06/2020.
+//  Copyright © 2020 Awislabs. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
